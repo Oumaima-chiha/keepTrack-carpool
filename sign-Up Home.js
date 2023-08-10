c6a26ba91b2c09279294a63605a74444eb2503a8
Oumaima-chiha <chihaouma@gmail.com>
2023-08-10 20:22:21 +0100

    added About and contact

---
+++ sign-Up Home.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+            $("#driver-button").click(function() {
+                window.location.href = "./sign-up.html";
+            });
+
+            $("#passenger-button").click(function() {
+                window.location.href = "./signUpp.html";
+            });
+        });
