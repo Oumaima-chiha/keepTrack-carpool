e652c1e9619fb93fcdc61fc0080c02a9960a27e6
Oumaima-chiha <chihaouma@gmail.com>
2023-08-11 00:00:17 +0100

    offer ride function

--- keeptrack.js
+++ keeptrack.js
@@ -1,3 +1,18 @@
+function each(coll, func) {
+    if (Array.isArray(coll)) {
+      for (var i = 0; i < coll.length; i++) {
+        func(coll[i], i);
+      }
+    } else {
+      for (var key in coll) {
+        func(coll[key], key);
+      }
+    }
+  }
+
+
+
+
 var loggedUser={}
 var isLoggedIn=false
 $(document).ready(function() {
@@ -15,5 +30,61 @@ $(document).ready(function() {
 
         $("#from").val(toValue);
         $("#to").val(fromValue);
+
     });
+   
+    let ridePosts = [];
+     function displayRideCards() {
+        const rideCardsContainer = $("#rideCards");
+        rideCardsContainer.empty(); 
+
+        each(ridePosts,function(ridePost, index) {
+            const card = `
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <h5 class="card-title">${ridePost.from} to ${ridePost.to}</h5>
+                        <p class="card-text">Date: ${ridePost.date}, Time: ${ridePost.time}</p>
+                    </div>
+                </div>
+            `;
+            rideCardsContainer.append(card);
+        });
+    }
+
+    
+    if (localStorage.getItem("ridePosts")) {
+        ridePosts = JSON.parse(localStorage.getItem("ridePosts"));
+    }
+
+    
+    function offerRide() {
+        const from = $("#from").val();
+        const to = $("#to").val();
+        const date = $("#date").val();
+        const time = $("#time").val();
+        
+        const ridePost = {
+            from: from,
+            to: to,
+            date: date,
+            time: time
+        };
+
+        ridePosts.push(ridePost);
+        localStorage.setItem("ridePosts", JSON.stringify(ridePosts));
+
+        alert("Your ride has been offered!");
+
+        // Clear the form fields
+        $("#from, #to, #date, #time").val("");
+    }
+
+    
+    $("form").submit(function(event){
+        event.preventDefault()
+         offerRide();
+         displayRideCards();
+});
+
+
 });
